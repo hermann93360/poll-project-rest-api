c89dec66be2db25ab8cc26b8f81b2a0f8a61b963
Hermann <hkamguin@gmail.com>
2023-04-14 16:17:40 +0200

    initial commit

---
+++ src/main/java/com/live/pollprojectrestapi/PollProjectRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.live.pollprojectrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollProjectRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollProjectRestApiApplication.class, args);
+    }
+
+}
